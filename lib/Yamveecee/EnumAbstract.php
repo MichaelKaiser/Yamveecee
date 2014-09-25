b6758f3355dd5565ee52743e7d066fc24a1d14f9
Michael Kaiser <michaells@cytainment.de>
2014-09-25 17:45:36 +0200

    added revert mapping for enum classes

--- lib/Yamveecee/EnumAbstract.php
+++ lib/Yamveecee/EnumAbstract.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kaiser
- * Date: 12.09.14
- * Time: 07:53
- */
-
 namespace Yamveecee;
 
-
+/**
+ * Class EnumAbstract
+ * @package Yamveecee
+ */
 class EnumAbstract
 {
+    /**
+     * @param $value
+     *
+     * @return string
+     */
+    public static function getConstantNameForValue($value)
+    {
+        $constantMapping = array_flip(static::getDefinedConstants());
+        if (array_key_exists($value, $constantMapping)) {
+            return $constantMapping[$value];
+        }
+        return "constant for value " . $value . " is not defined";
+    }
 
-} 
+    /**
+     * @return array
+     */
+    private static function getDefinedConstants()
+    {
+        $reflectionClass = new \ReflectionClass(get_called_class());
+        return $reflectionClass->getConstants();
+    }
+}
